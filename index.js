284ef339c2e595aa1852e9684839e1427119e5d8
Lironos <99316664+Lironos@users.noreply.github.com>
2022-02-09 09:13:31 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const { Client, Message, MessageEmbed, Collection } = require("discord.js");
+const fs = require("fs");
+const client = new Client({
+    messageCacheLifetime: 60,
+    fetchAllMembers: false,
+    messageCacheMaxSize: 10,
+    restTimeOffset: 0,
+    restWsBridgetimeout: 100,
+    shards: "auto",
+    allowedMentions: {
+        parse: ["roles", "users", "everyone"],
+        repliedUser: true,
+    },
+    partials: ["MESSAGE", "CHANNEL", "REACTION"],
+    intents: 32767,
+});
+module.exports = client;
+
+const config = require("./config.json");
+const prefix = config.prefix;
+// Global Variables
+client.commands = new Collection();
+client.aliases = new Collection();
+client.events = new Collection();
+client.cooldowns = new Collection();
+client.slashCommands = new Collection();
+client.categories = fs.readdirSync("./commands/");
+
+// Initializing the project
+//Loading files, with the client variable like Command Handler, Event Handler, ...
+["command", "event", "slash_handler"].forEach((handler) => {
+    require(`./handler/${handler}`)(client)
+});
+
+
+client.login(config.token);
+
